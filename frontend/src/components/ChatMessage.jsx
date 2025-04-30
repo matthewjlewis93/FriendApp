4a9db8c6bf820ea0139ed871834bfe1480bc991f
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-04-30 01:59:28 -0500

    better chat layout

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -1,23 +1,19 @@
 export default function ChatMessage({
   messageContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu mattis quam, sit amet faucibus sem. Sed in dictum eros. Vivamus massa sem, cursus vitae cursus ac, commodo eget enim. Nullam at ullamcorper enim. Mauris leo diam, egestas at mollis eu, euismod eu tellus. Vivamus porttitor nisi a ultrices facilisis. In elementum eros et nunc congue, ut commodo dolor finibus. Etiam diam leo, tincidunt non nisl eget, elementum sollicitudin arcu. Sed consequat eros placerat, ultrices lectus sed, tincidunt enim.",
-  messageReceived=false,
-  timeSent="2:58 PM"
+  messageReceived = false,
+  timeSent = "2:58 PM",
 }) {
   return (
-    <div className="chat-div"
-    >
+    <div className={"chat-div " + (messageReceived ? "received-chat" : "sent-chat")}>
       <p
-        className={
-          "chat-message " + (messageReceived ? "received-chat" : "sent-chat")
-        }
+        className="chat-message"
         style={{
           margin: "0",
         }}
       >
         {messageContent}
-        <div><p className="message-time">{timeSent}</p></div>
       </p>
-      {/* <p className="message-time">{timeSent}</p> */}
+      <p className="message-time">{timeSent}</p>
     </div>
   );
 }
