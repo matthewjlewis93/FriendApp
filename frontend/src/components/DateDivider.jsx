e9a27424000c2f423f84cb47eaef70cb5d59cda6
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-06-04 15:55:13 -0500

    sticky chat dates

--- frontend/src/components/DateDivider.jsx
+++ frontend/src/components/DateDivider.jsx
@@ -1,8 +1,8 @@
 export default function DateDivider({ dateString }) {
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
+    <div style={{ display: "flex", alignItems: "center", position: "sticky", top: '0px', margin: "10px -12px", padding: "0 12px", backgroundColor: "var(--chat-background)" }}>
       <hr color="#1b2631" style={{ flexGrow: 1 }} />
-      <h2 style={{ padding: "0px 10px" }}>{dateString}</h2>
+      <h2 style={{ padding: "0px 10px", margin: 0, fontSize: "1.2rem", letterSpacing: "4px" }}>{dateString}</h2>
       <hr color="#1b2631" style={{ flexGrow: 1 }} />
     </div>
   );
